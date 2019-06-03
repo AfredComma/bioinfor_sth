9fba09d3ade7ed443bd3ee1caf7b81f933d33f6d
commadou <commadou@163.com>
2019-06-03 15:09:34 +0800

    A: add the function of calculating shannon index,based on An integrated catalog of reference genes in the human gut microbiome

---
+++ estimate_alpha.R
@@ -0,0 +1,19 @@
+suppressPackageStartupMessages(library(argparse))
+suppressPackageStartupMessages(library(vegan))
+
+parser <- ArgumentParser()
+parser$add_argument("-i", help = "input otufile or abundance to calculate shannon")
+args <- parser$parse_args()
+
+infile <- file.path(args$i)
+
+ddf<-read.csv(infile,sep = '\t',row.names=1)
+df<-t(ddf)
+#es_data<-estimateR(df)
+shannon<-diversity(df,index="shannon")
+
+simpson<-diversity(df,index="simpson")
+es_data<-rbind(shannon,simpson)
+outfile<-sub("gene_relative_abun","alpha",infile)
+write.csv(es_data,file = outfile)
+
