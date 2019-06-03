15bb9fc7dad2fa86f3af45e5bd986b7f4767c7f6
commadou <commadou@163.com>
2019-06-03 13:07:34 +0800

    M: add the function of split and calculate relative abundance

--- split_columns.py
+++ split_columns.py
@@ -28,9 +28,29 @@ def ss(df, k):
         dfone.to_csv(out_file, sep='\t')
 
 
+def relative_abundance(df):
+    return df / df.sum(axis=0)
+
+
+def split_trans_abun(df, k):
+    in_list = df.columns.to_list()
+    n = len(in_list)
+    a = list(range(n))
+    b = a[::k]
+    c = [i for i in b[1:]]
+    c.append(a[-1] + 1)
+    nn = len(b)
+    for i in range(nn):
+        w = in_list[b[i]:c[i]]
+        df_two = relative_abundance(df[w])
+        print(df_two.head())
+        out_file = str(i) + '_gene_relative_abun.tsv'
+        df_two.to_csv(out_file, sep='\t')
+
+
 def main(infile):
     df = pd.read_csv(infile, sep='\t', index_col=0)
-    ss(df, 10)
+    split_trans_abun(df, 10)
 
 
 if __name__ == '__main__':
