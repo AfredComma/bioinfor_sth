5574c78cc1135ba086dec41b9ea772cf496ee976
commadou <commadou@163.com>
2019-06-04 10:22:53 +0800

    M: add the function of translate houmiao's file to qc shiny system

---
+++ qc_shiny_data_prepare.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @License : Copyright(c) Aegicare License
+# @Time    : 2019/6/4 上午9:54
+# @Author  : commadou
+# @Mail    : commadou@163.com
+# @File    : qc_shiny_data_prepare.py
+# @Software: PyCharm
+
+import os
+import sys
+import pandas as pd
+
+
+def p2f(x):
+    return float(x.strip('%')) / 100
+
+
+def read_seek(infile):
+    df = pd.read_csv(infile, sep='\t',
+                     converters={'covarage>1x': p2f, 'coverage>20x': p2f, 'coverage>30x': p2f, 'coverage>50x': p2f})
+    use_col = "sample,total,dup,capture efficiency,alignment,covarage>1x,coverage>20x,coverage>30x,coverage>50x,depth,gender,time".split(
+        ',')
+    df_use = df[use_col]
+    ids = df_use['sample'].to_list()
+    groups_list = []
+    for i in ids:
+        if 'AS' in i:
+            groups_list.append('production')
+        else:
+            groups_list.append('others')
+    df_use['delete'] = 0
+    df_use['group'] = groups_list
+    df_use['gender'] = df_use['gender'].fillna('Uncertain')
+    r = 'sample,total,dup,capture efficiency,alignment,covarage>1x,coverage>20x,coverage>30x,coverage>50x,depth,gender,time'.split(
+        ',')
+    s = 'sample,volume,dup_perc,specificity,align_rate,coverage>1,coverage>20,coverage>30,coverage>50,depth,gender,time'.split(
+        ',')
+    df_use.rename(columns=dict(zip(r, s)), inplace=True)
+    return df_use.head()
+
+
+def main(infile):
+    df_use = read_seek(infile)
+    df_use.to_csv("test.csv", index=False)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1])
