0890cff6c49fb081a78520c1a3fd9c37a30acef8
commadou <commadou@163.com>
2019-07-15 09:46:19 +0800

    M: add the function of download fasta

--- download_virus_protein_from_ncbi.py
+++ download_virus_protein_from_ncbi.py
@@ -13,7 +13,7 @@ import time
 from Bio import Entrez
 
 
-def get_count(search_text="viridae[Organism]", email='comdaou@126.com'):
+def get_count(search_text="viridae[Organism]", email='commadou@163.com'):
     Entrez.email = email
     bandle = Entrez.esearch(db="protein", term=search_text, idtype="acc")
     brecord = Entrez.read(bandle)
@@ -21,7 +21,7 @@ def get_count(search_text="viridae[Organism]", email='comdaou@126.com'):
     return int(brecord["Count"])
 
 
-def main(seq_num=100000,search_text="viridae[Organism]"):
+def main(seq_num=100000, search_text="viridae[Organism]"):
     number = get_count()
     all_id = []
     for i in range(0, number, seq_num):
@@ -38,5 +38,21 @@ def main(seq_num=100000,search_text="viridae[Organism]"):
         f.writelines(all_id)
 
 
+def download_dfata(infile):
+    Entrez.email = "commadou@163.com"
+    with open(infile, 'r') as f:
+        ids = f.readlines()
+    ids = [i.replace('\n', '') for i in ids]
+    for seq_id in ids:
+        outfile = str(seq_id) + ".fasta"
+        handle = Entrez.efetch(db="protein", id=seq_id, rettype="fasta", retmode="text")
+        record = handle.read()
+        re_string = record.rstrip('\n')
+        with open(outfile, 'w') as g:
+            g.write(re_string)
+        time.sleep(0.3)
+
+
 if __name__ == '__main__':
-    main()
+    # main()
+    download_dfata("Virus_protein_ncbi13_Jul_2019_17_30_46.txt")
