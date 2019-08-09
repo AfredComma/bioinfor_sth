722f5089078a6a31a6d70c5d193b365441f5bac5
AfredComma <commadou@163.com>
2019-08-09 14:32:53 +0800

    M: update the chao2 index for alpha

--- estimate_alpha.R
+++ estimate_alpha.R
@@ -20,3 +20,8 @@ write.csv(es_data,file = outfile)
 #however, I use the spceies dataframe instead to calculate the simpson value
 #simpson<-diversity(t(a),index="simpson")
 
+#In the IGC paper, we can see the evaluation of alpha diversity by chao2 index.
+#So we use the same method, with fossil package in R
+#library(fossil)
+#df<-read.csv("gene_count.tsv", sep = '\t', row.names = 1)
+#ss<-spp.est(df, rand = 10, abund = FALSE, counter = FALSE, max.est = 'all')
